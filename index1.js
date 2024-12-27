913d87711b60156c45957d4735a9b895d1e403d2
KP081 <ptlkathan2@gmail.com>
2024-12-27 11:07:46 +0530

    complet sync procces.

--- index1.js
+++ index1.js
@@ -1,5 +1,6 @@
 const bodyParser = require("body-parser");
 const express = require("express");
+const request = require("request");
 const { Blockchain } = require("./blockchain");
 const { PubSub } = require("./publishSuscribe");
 
@@ -8,6 +9,8 @@ const app = express();
 const blockchain = new Blockchain();
 const pubsub = new PubSub({ blockchain });
 
+const DEFAULT_PORT = 3000;
+const ROOT_NODE_ADDRESS = `http://localhost:${DEFAULT_PORT}`;
 setTimeout(() => pubsub.broadcastChain() , 1000);
 
 app.use(bodyParser.json());
@@ -24,7 +27,18 @@ app.post("/api/mine22" , (req , res) => {
     res.redirect("/api/blocks22");
 }); 
 
-const DEFAULT_PORT = 3000;
+const synChain = () => {
+    request({url:`${ROOT_NODE_ADDRESS}/api/blocks22`} ,
+         (error , response , body) => {
+            if(!error && response.statusCode === 200){
+            const rootChain = JSON.parse(body);
+            console.log("replace chain on sync with : ",rootChain);
+            blockchain.replaceChain(rootChain);
+            }
+        }
+    );
+};
+
 let PEER_PORT;
 
 if(process.env.GENERATE_PEER_PORT === "true") {
@@ -34,4 +48,5 @@ const PORT = PEER_PORT || DEFAULT_PORT;
 
 app.listen(PORT , () => {
     console.log(`listening to PORT : ${PORT}`);
+    synChain();
 }); 
